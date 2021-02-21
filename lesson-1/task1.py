2fbbb0e21c7dc16c9e66d4f6f8907e2150a4f0a3
Dmintry <dumasik343@gmail.com>
2021-02-21 12:33:44 +0300

    Решение первой задачи

---
+++ lesson-1/task1.py
@@ -0,0 +1,15 @@
+
+sec = int(input("Введите время в секундах"))
+h = sec // 3600
+m = (sec - h*3600)//60
+s = sec % 60
+counts = 0
+while counts < sec:
+    print(h, "час", m, "мин", s, "сек")
+    h = sec // 3600
+    m = (sec - h * 3600) // 60
+    s = sec % 60
+    sec = int(input("Введите время в секундах"))
+
+
+
