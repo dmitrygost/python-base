87ce7945855f292ab9c0ee6c74130b24dcf7a4c0
Dmintry <dumasik343@gmail.com>
2021-03-01 22:19:38 +0300

    Решение 1 задачи

--- lesson-1/task1.py
+++ lesson-1/task1.py
@@ -1,15 +1,21 @@
+# sec = int(input("Введите время в секундах"))
+# h = sec // 3600
+# m = (sec - h*3600)//60
+# s = sec % 60
+# counts = 0
+# while counts < sec:
+#     print(h, "час", m, "мин", s, "сек")
+#     h = sec // 3600
+#     m = (sec - h * 3600) // 60
+#     s = sec % 60
+#     sec = int(input("Введите время в секундах"))
 
-sec = int(input("Введите время в секундах"))
-h = sec // 3600
-m = (sec - h*3600)//60
-s = sec % 60
-counts = 0
-while counts < sec:
-    print(h, "час", m, "мин", s, "сек")
-    h = sec // 3600
-    m = (sec - h * 3600) // 60
-    s = sec % 60
-    sec = int(input("Введите время в секундах"))
+a =(1, 11)
+for x in a:
+    print(a)
+
+
+
 
 
 
---
+++ lesson-2/task1.py
@@ -0,0 +1,7 @@
+
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+print(a, type(a), b, type(b), c, type(c), d, type(d))
+
