28234f9e688ea666377b792dc493bbdd63c41026
Dmintry <dumasik343@gmail.com>
2021-03-01 22:49:07 +0300

    Решение 1 задачи

--- lesson-2/task1.py
+++ lesson-2/task1.py
@@ -1,7 +1,14 @@
-
+#простое решение
 a = 15 * 3
 b = 15 / 3
 c = 15 // 2
 d = 15 ** 2
 print(a, type(a), b, type(b), c, type(c), d, type(d))
+
+#или
+
+print(f'{type(15 * 3)=}')
+print(f'{type(15 / 3)=}')
+print(f'{type(15 // 2)=}')
+print(f'{type(15 ** 2)=}')
 
---
+++ lesson-2/task2.py
@@ -0,0 +1,9 @@
+
+info_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+info_list.insert(1, "")
+info_list.insert(3, "")
+info_list.insert(5, "")
+info_list.insert(7, "")
+info_list.insert(12, "")
+info_list.insert(14, "")
+print(info_list)
