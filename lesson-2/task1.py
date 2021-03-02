64f8241ac744faad769a8a65eeeb106d738da223
Dmintry <dumasik343@gmail.com>
2021-03-02 22:06:16 +0300

    Решение 1 задачи

--- lesson-2/task1.py
+++ lesson-2/task1.py
@@ -5,10 +5,7 @@ c = 15 // 2
 d = 15 ** 2
 print(a, type(a), b, type(b), c, type(c), d, type(d))
 
-#или
 
-print(f'{type(15 * 3)=}')
-print(f'{type(15 / 3)=}')
-print(f'{type(15 // 2)=}')
-print(f'{type(15 ** 2)=}')
+
+
 
