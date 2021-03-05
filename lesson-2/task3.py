3c34c3f4afe70c43d4bfdfd76c4fc8c8ed816a96
Dmintry <dumasik343@gmail.com>
2021-03-05 14:27:56 +0300

    Решение 3 задачи

---
+++ lesson-2/task3.py
@@ -0,0 +1,17 @@
+
+staff = ['инженер-конструктор Игорь',
+         'главный бухгалтер МАРИНА',
+         'токарь высшего разряда нИКОЛАЙ',
+         'директор аэлита'
+]
+for first_name in staff:
+    first_name = first_name.split()
+    name = first_name[-1].title()
+    print('Привет,', name!)
+
+
+
+
+
+
+
