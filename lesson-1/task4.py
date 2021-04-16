8c3a324aec7233411449676c2cafa21510b3ebd0
dmitrygost <79397046+dmitrygost@users.noreply.github.com>
2021-04-16 09:18:41 +0300

    Update task4.py

--- lesson-1/task4.py
+++ lesson-1/task4.py
@@ -1,1 +1,15 @@
+for n in range(0, 22):
+    num = n % 10
+    if n == 0:
+        print(n, 'процентов')
+    elif num == 0:
+        print(n, 'процентов')
+    elif n >= 11 and n <= 14:
+        print(n, 'процентов')
+    elif num == 1:
+        print(n, 'процент')
+    elif num >= 5:
+        print(n, 'процентов')
+    else:
+        print(n, 'процента')
 
